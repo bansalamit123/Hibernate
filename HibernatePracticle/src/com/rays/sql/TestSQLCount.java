5e0d1d31f000fd87b9c2647762c26a556f73cfc2
bansalamit123 <bansalamit168@gmail.com>
2024-02-23 12:53:28 +0530

    done

---
+++ HibernatePracticle/src/com/rays/sql/TestSQLCount.java
@@ -0,0 +1,40 @@
+package com.rays.sql;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.SQLQuery;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.classic.Session;
+
+public class TestSQLCount {
+	public static void main(String[] args) {
+
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
+		Session session = sf.openSession();
+
+		Transaction tx = session.beginTransaction();
+
+		SQLQuery q = session.createSQLQuery("select count(*) from user");
+
+		List list = q.list();
+
+		Iterator it = list.iterator();
+
+		while (it.hasNext()) {
+
+			Object dto = (Object) it.next();
+
+			System.out.print(dto);
+		}
+
+		tx.commit();
+
+		session.close();
+
+	}
+
+}
